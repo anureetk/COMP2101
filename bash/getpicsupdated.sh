ac7b96c104ec8b8210e6ac5abcdf47dccf830d9d
anureetk <anureet2000@icloud.com>
2020-07-08 21:12:14 -0700

    getpics.sh update done

---
+++ bash/getpicsupdated.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# this script puts some picture files into a Pictures directory in our home directory
+# it does some error testing
+# it summarizes the Pictures directory when it is done
+#
+
+# Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
+#
+
+# Creating a pictures directory
+test -d ~/Pictures || mkdir ~/Pictures
+
+# downloading a zipfile of pictures to our Pictures directory we just created
+test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net/pics.zip
+
+# unpacking the downloaded zipfile if it is there, then delete the local copy of the zipfile
+test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
+
+# Making a report on what we is in there Pictures directory
+test -d ~/Pictures && cat <<EOF
+Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
+The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
+EOF
+#done
