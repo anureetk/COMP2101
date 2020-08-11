1763c140c6c9588e4d7a710466c5d6b0634e8c0e
anureetk <65686328+anureetk@users.noreply.github.com>
2020-08-11 16:45:39 -0700

    LAB2

---
+++ Powershell/cpuinfo.ps1
@@ -0,0 +1,8 @@
+﻿#creating a function for cpu info
+
+function get-cpuinfo{
+write-output "cpu information"
+$info = get-ciminstance -class win32_process | format-list *Manufacturer,NUmberofCores*
+write-output $info}
+
+get-cpuinfo
---
+++ Powershell/welcome.ps1
@@ -0,0 +1,8 @@
+﻿#welcome script for LAB2
+
+function welcome {
+write-output "Welcome to planet $env:computername Overlord $env:username"
+$now = get-date -format 'HH:MM tt on dddd'
+write-output "It is $now."}
+
+welcome
