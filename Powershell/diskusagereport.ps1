5fa6c4adca371edc0cb8fac4224ce2cadfc5710d
anureetk <65686328+anureetk@users.noreply.github.com>
2020-08-11 18:08:49 -0700

    LAB3

---
+++ Powershell/diskusagereport.ps1
@@ -0,0 +1,7 @@
+﻿
+write-output "Powershell disk usage report"
+gwmi -class win32_logicaldisk | where-object size -gt 0 | 
+format-table -autosize @{Label="Filesystem Drive";Expression={$_.deviceid[0]}},
+@{Label="Size(GB)";Expression={$_.size/1gb -as [int]}},
+@{Label="Free space(GB)";Expression={$_.freespace/1gb -as [int]}},
+ProviderName
---
+++ Powershell/softwareReport.ps1
@@ -0,0 +1,7 @@
+﻿#report for non-microsoft software installed on the system
+
+write-output "Report for non-microsoft software installed on the system"
+
+gwmi -class win32_product -filter "vendor!='Microsoft Corporation'" | 
+sort-object InstallDate | select-object name,vendor,version,InstallDate | 
+format-table -autosize
