31a3495918e75bce645a75d3759ad01fcebd44e7
anureetk <65686328+anureetk@users.noreply.github.com>
2020-08-11 19:42:08 -0700

    LAB5

---
+++ Powershell/Parameters.ps1
@@ -0,0 +1,9 @@
+﻿
+Param ([Parameter(Mandatory=$true,
+position=1,
+ValueFromPipeline=$true,
+ValueFromPipelineByPropertyName=$true)]$ObjectName)
+
+"Object name was '$ObjectName'"
+$objTypeName = $ObjectName.gettype().name
+"$ObjectName type is $Objtypename object"
